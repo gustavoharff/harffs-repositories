17125e756ea2b267d25c3b08ca7cd50ff9386a11
Gustavo Harff <gustavo.harff@gmail.com>
2020-09-28 11:11:38 -0300

    chore: repos styles

--- src/styles/pages/repos.ts
+++ src/styles/pages/repos.ts
@@ -22,32 +22,44 @@ export const Content = styled.div`
   justify-content: space-between;
   align-items: center;
 
-  li {
-    width: 400px;
-    height: 120px;
-    background: #383838;
-    margin: 20px auto;
-    border: 2px solid #717171;
-    border-radius: 10px;
+  ul {
+    margin: 0 auto;
     display: flex;
-    color: ${props => props.theme.colors.text};
-    flex-direction: column;
+    width: 100%;
     align-items: center;
-    justify-content: space-between;
-    text-align: center;
-    transition: 0.2s;
+    justify-content: space-around;
+    flex-grow: 1;
+    flex-wrap: wrap;
+    padding: 0 20px;
 
-    &:hover {
-      background: ${props => props.theme.colors.secondaryGreenHover};
-      border-color: ${props => props.theme.colors.primaryGreenHover};
-    }
+    li {
+      width: 400px;
+      height: 120px;
+      background: none;
+      margin: 20px auto;
+      border: 1px solid #5f6368;
+      border-radius: 10px;
+      display: flex;
+      color: ${props => props.theme.colors.text};
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+      text-align: center;
+      transition: 0.2s;
+      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.75);
 
-    strong {
-      margin: 5px 15px;
-    }
+      &:hover {
+        background: ${props => props.theme.colors.secondaryGreenHover};
+        border-color: ${props => props.theme.colors.primaryGreenHover};
+      }
 
-    p {
-      margin: 0px 5px 5px 15px;
+      strong {
+        margin: 5px 15px;
+      }
+
+      p {
+        margin: 0px 5px 5px 15px;
+      }
     }
   }
 `
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { darken } from 'polished'
 
 const theme = {
   colors: {
-    background: '#121214',
+    background: '#191622',
     text: '#D3D3D3',
     primaryGreen: '#2CB436',
     primaryGreenHover: darken(0.2, '#2CB436'),
